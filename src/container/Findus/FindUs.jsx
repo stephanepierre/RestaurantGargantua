4e3723f719a527ba8feba1c62bd0bf13d7f3dba8
stephanepierre <stephane_pierre@hotmail.fr>
2022-03-18 10:48:19 +0100

    ADD footer

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -12,7 +12,7 @@ const FindUs = () => (
         <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id possimus deleniti aperiam laudantium repudiandae est enim exercitationem expedita nisi asperiores!</p>
         <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Horaires</p>
         <p className="p__opensans">Lundi - Vendredi: 10:00 - 22:00</p>
-        <p className="p__opensans">Samedi - Dimanche: 10:00 - 01:00</p>
+        <p className="p__opensans">Samedi - Dimanche: 07:00 - 23:00</p>
       </div>
       <button type="button" className="custom__button" style={{ marginTop: '2rem'}}>Nous trouver</button>
     </div>
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
-
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
+import { images } from '../../constants';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact</h1>
+        <p className="p__opensans">2 Rue de la chaussée, 75000 Paris, France</p>
+        <p className="p__opensans">01 01 01 01 01</p>
+        <p className="p__opensans">01 02 02 02 02</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="logo restaurant" />
+        <p className="p__opensans">"La meilleure façon de se trouver est de se perdre au service des autres."</p>
+        <img src={images.spoon} alt="spoon" className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Ouvertures</h1>
+        <p className="p__opensans">Lundi - Vendredi</p>
+        <p className="p__opensans">10:00 - 22:00</p>
+        <p className="p__opensans">Samedi - Dimanche</p>
+        <p className="p__opensans">07:00 - 23:00</p>
+      </div>
+
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Gargantua. All rights reserved</p>
+    </div>
   </div>
 );
 
